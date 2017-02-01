2b5539973d230a5ceee6ebdee0544db49bbcda20
Alexis Grant <alexisg@gmail.com>
2017-01-31 16:51:51 -0800

    omg CORS omg

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org' 
 ruby '2.4.0' 
 gem 'sinatra' 
-gem 'rack-cors', :require => 'rack/cors'
+gem "sinatra-cross_origin", "~> 0.3.1"
 gem 'thin'
--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+
+set :allow_origin, :any
+set :allow_methods, [:get, :post, :options]
+set :expose_headers, ['Content-Type']
 
 get '/' do
   'Hello world!'
@@ -9,7 +14,12 @@ get '/log' do
 end
 
 options '/log' do
+ cross_origin
+ response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+ response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+ 200
 end
 
 post '/log' do
+ cross_origin
 end
