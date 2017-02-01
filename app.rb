32bc7ddc6cf69e2cd2ab3af5f5b8127467e4fbb5
Alexis Grant <alexisg@gmail.com>
2017-02-01 10:02:23 -0800

    log params to logger neatly, make app more explanatory.

--- app.rb
+++ app.rb
@@ -6,21 +6,26 @@ set :allow_methods, [:get, :post, :options]
 set :expose_headers, ['Content-Type']
 
 get '/' do
-  'Hello world!'
+  'This is a logger bridge app. GET /log for details.'
 end
 
 get '/log' do
- 'Try logging a thing'
+ 'This app accepts POST requests to /log from client-side apps. Try it!'
 end
 
 options '/log' do
  cross_origin
+
+ # Works around a bug in OPTIONS handling with sinatra/cross-origin
  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+
  200
 end
 
 post '/log' do
  cross_origin
- logger.info request.params
+ request.params.console_log.each do |param|
+   logger.info(param)
+ end
 end
